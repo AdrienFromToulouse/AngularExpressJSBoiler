1f7749d89efe2756058c6f2d4d195dcc6ea8d4e5
Adrien Desbiaux <adrien.desbiaux@gmail.com>
2013-07-17 10:16:28 +0900

    init

--- public/js/controllers.coffee
+++ public/js/controllers.coffee
@@ -7,4 +7,22 @@ angular.module('myApp').controller 'AppCtrl', ($scope, $http) ->
   ).error (data, status, headers, config) ->
     $scope.name = "Error!"
 
+angular.module('myApp').controller 'MyCtrl1', ($scope, $http) ->
+  $http(
+    method: "GET"
+    url: "/api/name"
+  ).success((data, status, headers, config) ->
+    $scope.name = data.name
+  ).error (data, status, headers, config) ->
+    $scope.name = "Error!"
+
+angular.module('myApp').controller 'MyCtrl2', ($scope, $http) ->
+  $http(
+    method: "GET"
+    url: "/api/name"
+  ).success((data, status, headers, config) ->
+    $scope.name = data.name
+  ).error (data, status, headers, config) ->
+    $scope.name = "Error!"
+
   
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -11,4 +11,26 @@
     });
   });
 
+  angular.module('myApp').controller('MyCtrl1', function($scope, $http) {
+    return $http({
+      method: "GET",
+      url: "/api/name"
+    }).success(function(data, status, headers, config) {
+      return $scope.name = data.name;
+    }).error(function(data, status, headers, config) {
+      return $scope.name = "Error!";
+    });
+  });
+
+  angular.module('myApp').controller('MyCtrl2', function($scope, $http) {
+    return $http({
+      method: "GET",
+      url: "/api/name"
+    }).success(function(data, status, headers, config) {
+      return $scope.name = data.name;
+    }).error(function(data, status, headers, config) {
+      return $scope.name = "Error!";
+    });
+  });
+
 }).call(this);
