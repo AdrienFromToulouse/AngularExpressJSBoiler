940fbcd2f89b26d5f3805be57c4862ce86387671
Adrien Desbiaux <adrien.desbiaux@gmail.com>
2013-07-17 12:14:04 +0900

    init

---
+++ config.rb
@@ -0,0 +1,10 @@
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "public/css"
+sass_dir = "public/scss"
+images_dir = "public/images"
+javascripts_dir = "public/js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
